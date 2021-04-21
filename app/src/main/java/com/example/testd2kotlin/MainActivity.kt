37796e5630eb73c3bd88240f42c0a2948842c0e8
nikitkaf1 <nikitkaf1@gmail.com>
2021-04-21 16:13:54 +0400

    поправил контекст

--- app/src/main/java/com/example/testd2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/testd2kotlin/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.testd2kotlin
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
@@ -9,20 +8,16 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private lateinit var context: Context
-    }
     private val adapter = RecyclerAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        context = this
         val viewModel = MainViewModel()
         val recycler = findViewById<RecyclerView>(R.id.employees_rv)
         viewModel.loadData()
         viewModel.employees.observe(this, Observer {
             adapter.setEmployees(it)
-            recycler.layoutManager = LinearLayoutManager(context)
+            recycler.layoutManager = LinearLayoutManager(baseContext)
             recycler.adapter = adapter
         })
     }
